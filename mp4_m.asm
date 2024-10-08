c9cf49d6e86c37c2b3e8f859c8fbaf477ef90a3f
renrenrenz <durezarenz@gmail.com>
2024-10-08 20:53:01 +0800

    PLEASE

--- mp4_m.asm
+++ mp4_m.asm
@@ -3,18 +3,10 @@ section .data
 	clearScreen db 27, '[2J', 0
 
 	;Enrollment Status
-	moveCursor3 db 27 '[9;1H'
-	moveCursor3_len equ $ - moveCursor3
 	msgEnroll db 'Enter Enrollment Status: '
 	msgEnrollLen equ $ - msgEnroll
 	dspEnroll db 'ENROLLMENT STATUS: '
 	dspEnrollLen equ $ - dspEnroll
-	
-	;Term
-	msgTerm db 'Enter Term: ' 
-	msgTermLen equ $ - msgTerm
-	dspTerm db 'Term: '
-	dspTermLen equ $ - dspTerm
 	
 	;Student
 	msgStudNum db 'Enter Student no.: ' 
@@ -27,30 +19,46 @@ section .data
 	msgNameLen equ $ - msgName
 	dspName db 'Name: '
 	dspNameLen equ $ - dspName
+
+	;Term
+	moveCursor1 db 27, '[1:1H'
+	moveCursor1_len equ $ - moveCursor1
+	msgTerm db 'Enter Term: ' 
+	msgTermLen equ $ - msgTerm
+	dspTerm db 'Term: '
+	dspTermLen equ $ - dspTerm
 	
 	;School Year
+	moveCursor2 db 27, '[1:11H'
+	moveCursor2_len equ $ - moveCursor2
 	msgSchoolYear db 'Enter School Year: '
 	msgSchoolYearLen equ $ - msgSchoolYear
 	dspSchoolYear db 'School Year: '
 	dspSchoolYearLen equ $ - dspSchoolYear
 	
 	;College
+	moveCursor3 db 27, '[2:45H'
+	moveCursor3_len equ $ - moveCursor3
 	msgCollege db 'Enter College: '
 	msgCollegeLen equ $ - msgCollege
 	dspCollege db 'College: '
 	dspCollegeLen equ $ - dspCollege
 	
-	;Year Level
-	msgYearLevel db 'Enter Year Level: '
-	msgYearLevelLen equ $ - msgYearLevel
-	dspYearLevel db 'Year Level: '
-	dspYearLevelLen equ $ - dspYearLevel
-	
 	;Program
+	moveCursor4 db 27, '[2:73H'
+	moveCursor4_len equ $ - moveCursor4
 	msgProgram db 'Enter Program: '
 	msgProgramLen equ $ - msgProgram
 	dspProgram db 'Program: '
 	dspProgramLen equ $ - dspProgram
+
+	;Year Level
+	moveCursor5 db 27, '[3:45H'
+	moveCursor5_len equ $ - moveCursor5
+	msgYearLevel db 'Enter Year Level: '
+	msgYearLevelLen equ $ - msgYearLevel
+	dspYearLevel db 'Year Level: '
+	dspYearLevelLen equ $ - dspYearLevel
 
 	;cor
 	cor1 db 27, '[7;3H'          
@@ -118,31 +126,57 @@ section .data
 	cor4Len equ $ - cor4
 	
 	;fee
-	tuition db 27, '[12;3H'          
-    		db 'Tuition Fee(21)', 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, '43,218.00', 0xa,
-			db 27, '[13;3H' 
-    		db 'Miscellaneous Fee', 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, '9,865.00', 0xa,	
-			db 27, '[14;3H' 
-			db 'ID Validation Fee', 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, '78.00', 0xa,	
-			db 27, '[15;3H' 
-			db 'ITE Computer Laboratory Fee (CCS0021)', 0x9, 0x9, 0x9, 0x9, 0x9, '3,914.00', 0xa,	
-			db 27, '[16;3H' 
-			db 'ITE Computer Laboratory Fee (CS0070)', 0x9, 0x9, 0x9, 0x9, 0x9,  '3,914.00', 0xa,	
-			db 27, '[17;3H' 
-			db 'ITE Computer Laboratory Fee (CS0003)', 0x9, 0x9, 0x9, 0x9, 0x9,  '3,914.00', 0xa,
-			db 27, '[18;3H' 
-			db 'Science Lab Fee (GED0081)', 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, '4,007.00', 0xa,	     
-			db 27, '[19;3H' 
-			db 'TOTAL ASSESSMENT', 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, '68,910.00', 0xa, 0xa, 0xa, 0xa,
-			db 27, '[20;3H' 
-			db 'Down Payment (Upon Enrollment) 40%', 0x9, 0x9, 0x9, 0x9, 0x9, '27,564.00', 0xa,	 	 
-			db 27, '[21;3H'     		
-			db 'Midterm (Oct 14 - Oct 16 2024) 30%', 0x9, 0x9, 0x9, 0x9, 0x9, '20,673.00', 0xa,	
-			db 27, '[22;3H'  
-			db 'Final (Nov 29 - Dec 04 2024) 30%', 0x9, 0x9, 0x9, 0x9, 0x9, '20,673.00', 0xa,	
-			db 27, '[23;3H'  
-			db 'Add on: Installment Fee', 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, 0x9, '250.00', 0xa,
-	tuitionLen equ $ - tuition
+	fee1 db 27, '[12;3H'
+		db "Tuition Fee (21)"
+		db 27, '[13;3H'
+		db "Miscellaneous Fee"
+		db 27, '[14;3H'
+		db "ID Validation Fee"
+		db 27, '[15;3H'
+		db "ITE Computer Laboratory Fee (CS0070)"
+		db 27, '[16;3H'
+		db "ITE Computer Laboratory Fee (CS0003)"
+		db 27, '[17;3H'
+		db "ITE Computer Laboratory Fee (CCS0021)"
+		db 27, '[18;3H'
+		db "Science Lab Fee (GED0081)"
+		db 27, '[19;3H'
+		db "TOTAL ASSESSMENT"
+		db 27, '[21;3H'
+		db "Down Payment (Upon Enrollment) 40%"
+		db 27, '[22;3H'
+		db "Midterm (Oct 14 - Oct 16 2024) 30%"
+		db 27, '[23;3H'
+		db "Final (Nov 29 - Dec 04 2024) 30%"
+		db 27, '[24;3H'
+		db "Add on: Installment Fee"
+	fee1Len equ $ - fee1
+
+	fee2 db 27, '[12;50H'
+	    db "43,218.00"
+		db 27, '[13;50H'
+		db "9,865.00"
+		db 27, '[14;50H'
+		db "78.00"
+		db 27, '[15;50H'
+		db "3,914.00"
+		db 27, '[16;50H'
+		db "3,914.00"
+		db 27, '[17;50H'
+		db "3,914.00"
+		db 27, '[18;50H'
+		db "4,007.00"
+		db 27, '[19;50H'
+		db "68,910.00"
+		db 27, '[23;50H'
+		db "27,564.00"
+		db 27, '[24;50H'
+		db "20,673.00"
+		db 27, '[25;50H'
+		db "20,673.00"
+		db 27, '[26;50H'
+		db "250.00", 10
+	fee2Len equ $ - fee2
     	
 section .bss
 	enroll resb 50
@@ -206,6 +240,7 @@ _start:
 		int 0x80
 		ret
 	
+	;enroll
 	displayPromtEnroll:
 		mov eax, 4
 		mov ebx, 1
@@ -221,12 +256,6 @@ _start:
 		ret
 	
 	displayInputEnroll:
-		mov eax, 4	
-		mov ebx, 1
-		mov ecx, moveCursor3
-		mov edx, moveCursor3_len
-		int 0x80
-
 		mov eax, 4
 		mov ebx, 1
 		mov ecx, dspEnroll
@@ -240,6 +269,7 @@ _start:
 		int 0x80  
 		ret
    	
+	;term
    	displayPromtTerm:
 		mov eax, 4
 		mov ebx, 1
@@ -257,6 +287,11 @@ _start:
 	displayInputTerm:
 		mov eax, 4
 		mov ebx, 1
+		mov ecx, moveCursor1
+		mov edx, moveCursor1_len 
+
+		mov eax, 4
+		mov ebx, 1
 		mov ecx, dspTerm
 		mov edx, dspTermLen
 		int 0x80  
@@ -268,6 +303,42 @@ _start:
 		int 0x80
 		ret
    	
+	;schoolYear
+	displayPromtSchoolYear:
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, msgSchoolYear
+		mov edx, msgSchoolYearLen
+		int 0x80
+
+		mov eax, 3
+		mov ebx, 0
+		mov ecx, schoolYear  
+		mov edx, schoolYearLen          
+		int 0x80
+		ret
+		
+   	displayInputSchoolYear:
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, moveCursor2
+		mov edx, moveCursor2_len
+		int 0x80
+
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, dspSchoolYear
+		mov edx, dspSchoolYearLen
+		int 0x80  
+
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, schoolYear
+		mov edx, schoolYearLen
+		int 0x80
+		ret
+
+	;studNum
    	displayPromtStudNum:
 		mov eax, 4
 		mov ebx, 1
@@ -293,6 +364,75 @@ _start:
 		mov ebx, 1
 		mov ecx, studNum
 		mov edx, studNumLen
+		int 0x80
+		ret
+
+	;college
+	displayPromtCollege:
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, msgCollege
+		mov edx, msgCollegeLen
+		int 0x80
+
+		mov eax, 3
+		mov ebx, 0
+		mov ecx, college  
+		mov edx, collegeLen          
+		int 0x80
+		ret
+    
+   	displayInputCollege:
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, moveCursor3
+		mov edx, moveCursor3_len
+		int 0x80
+
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, dspCollege
+		mov edx, dspCollegeLen
+		int 0x80  
+
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, college
+		mov edx, collegeLen
+		int 0x80
+		ret
+
+	;program
+	displayPromtProgram:
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, msgProgram
+		mov edx, msgProgramLen
+		int 0x80
+
+		mov eax, 3
+		mov ebx, 0
+		mov ecx, program  
+		mov edx, programLen          
+		int 0x80
+		ret
+    
+   	displayInputProgram:
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, moveCursor4
+		mov edx, moveCursor4_len
+
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, dspProgram
+		mov edx, dspProgramLen
+		int 0x80  
+
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, program
+		mov edx, programLen
 		int 0x80
 		ret
    	
@@ -323,64 +463,9 @@ _start:
 		mov ecx, name
 		mov edx, nameLen
 		int 0x80
-		ret 
+		ret  
     
-   	displayPromtSchoolYear:
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, msgSchoolYear
-		mov edx, msgSchoolYearLen
-		int 0x80
-
-		mov eax, 3
-		mov ebx, 0
-		mov ecx, schoolYear  
-		mov edx, schoolYearLen          
-		int 0x80
-		ret
-		
-   	displayInputSchoolYear:
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, dspSchoolYear
-		mov edx, dspSchoolYearLen
-		int 0x80  
-
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, schoolYear
-		mov edx, schoolYearLen
-		int 0x80
-		ret 
-    
-	displayPromtCollege:
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, msgCollege
-		mov edx, msgCollegeLen
-		int 0x80
-
-		mov eax, 3
-		mov ebx, 0
-		mov ecx, college  
-		mov edx, collegeLen          
-		int 0x80
-		ret
-    
-   	displayInputCollege:
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, dspCollege
-		mov edx, dspCollegeLen
-		int 0x80  
-
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, college
-		mov edx, collegeLen
-		int 0x80
-		ret 
-    
+	;yeasLevel
 	displayPromtYearLevel:
 		mov eax, 4
 		mov ebx, 1
@@ -398,6 +483,11 @@ _start:
    	displayInputYearLevel:
 		mov eax, 4
 		mov ebx, 1
+		mov ecx, moveCursor5
+		mov edx, moveCursor5_len
+
+		mov eax, 4
+		mov ebx, 1
 		mov ecx, dspYearLevel
 		mov edx, dspYearLevelLen
 		int 0x80  
@@ -408,34 +498,6 @@ _start:
 		mov edx, yearLevelLen
 		int 0x80
 		ret 
-    
-	displayPromtProgram:
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, msgProgram
-		mov edx, msgProgramLen
-		int 0x80
-
-		mov eax, 3
-		mov ebx, 0
-		mov ecx, program  
-		mov edx, programLen          
-		int 0x80
-		ret
-    
-   	displayInputProgram:
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, dspProgram
-		mov edx, dspProgramLen
-		int 0x80  
-
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, program
-		mov edx, programLen
-		int 0x80
-		ret
 
     displayCOR:
 		mov eax, 4
@@ -466,10 +528,16 @@ _start:
     displayFees:
 		mov eax, 4
 		mov ebx, 1
-		mov ecx, tuition
-		mov edx, tuitionLen
+		mov ecx, fee1
+		mov edx, fee1_len
+		int 0x80
+		
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, fee2
+		mov edx, fee2_len
 		int 0x80
 		ret
 
-    mov eax, 1
+	mov eax, 1
     int 0x80
